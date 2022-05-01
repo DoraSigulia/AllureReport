043895207a0269613eaf09bb4312ec876ced7f6b
Daria <sigulia39@gmail.com>
2022-05-01 19:53:56 +0300

    2. Лямбда шаги через step (name, () -> {})

--- src/test/java/com/sigulia/test/SelenideIssue.java
+++ src/test/java/com/sigulia/test/SelenideIssue.java
@@ -6,28 +6,54 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.partialLinkText;
 
 public class SelenideIssue {
 
     SelenideElement searchInput = $(".header-search-input");
-    String nameRepository = "DoraSigulia/DemoQA_Sigulia",
+    String  nameWebsite = "https://github.com/",
+            nameRepository = "DoraSigulia/DemoQA_Sigulia",
             namePage = "Issue",
             numberIssue = "#2";
 
 
     @Test
-    public void testGithubIssue () {
+    public void testGithubIssueListener () {
         SelenideLogger.addListener("allure", new AllureSelenide());
-        open("https://github.com/");
-        searchInput.click();
-        searchInput.sendKeys(nameRepository);
-        searchInput.submit();
+        open(nameWebsite);
+        searchNameRepository(nameRepository);
         $(linkText(nameRepository)).click();
         $(partialLinkText(namePage)).click();
         $(withText(numberIssue)).click();
 
-
     }
+
+    @Test
+    public void testGithubIssueLambda () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем главную страницу" + nameWebsite, () -> {
+            open(nameWebsite);
+        });
+        step("Находим репозиторий " + nameRepository, () -> {
+            searchNameRepository(nameRepository);
+        });
+        step("Переходим на репозиторий " + nameRepository, () -> {
+            $(linkText(nameRepository)).click();
+        });
+        step("Переходим на вкладку " + namePage, () -> {
+            $(partialLinkText(namePage)).click();
+        });
+        step("Переходим на " + namePage + " с номером" + numberIssue, () -> {
+            $(withText(numberIssue)).click();
+        });
+    }
+
+    private void searchNameRepository(String nameRepository) {
+        searchInput.click();
+        searchInput.sendKeys(nameRepository);
+        searchInput.submit();
+    }
+
 }
