9673a08e4b3916a126d4bb10be2e5182e31817e9
Daria <sigulia39@gmail.com>
2022-05-02 22:10:52 +0300

    add Attachment

--- src/test/java/com/sigulia/pages/WebSteps.java
+++ src/test/java/com/sigulia/pages/WebSteps.java
@@ -1,6 +1,11 @@
 package com.sigulia.pages;
-
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.nio.charset.StandardCharsets;
 
 public class WebSteps {
 
@@ -9,20 +14,39 @@ public class WebSteps {
     @Step("Открываем главную страницу {nameWebsite}")
     public void openPage (String nameWebsite) {
         githubTest.openPage(nameWebsite);
+        attachScreenshot();
+        attachPageSource();
     }
 
     @Step("Находим и переходим на репозиторий {nameRepository}")
     public void searchNameRepository (String nameRepository) {
         githubTest.searchNameRepository(nameRepository);
+        attachScreenshot();
+        attachPageSource();
     }
 
     @Step("Переходим на вкладку {namePage}")
     public void clickOnIssue (String namePage) {
         githubTest.clickOnIssue(namePage);
+        attachScreenshot();
+        attachPageSource();
     }
 
     @Step("Проверяем наличие Issue с номером {numberIssue}")
     public void visibleNumberIssue(String numberIssue) {
         githubTest.visibleNumberIssue(numberIssue);
+        attachScreenshot();
+        attachPageSource();
     }
+
+    @Attachment(value = "Скриншот", type = "image/png", fileExtension = "png")
+    public byte[] attachScreenshot () {
+        return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Attachment(value = "PageSource", type = "text/html", fileExtension = "html")
+    public byte[] attachPageSource () {
+        return WebDriverRunner.getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+    }
+
 }
